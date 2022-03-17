2587ca0470da88a6b288790005e21ff015b90fbc
sivhaga <sivhaga@hotmail.com>
2022-03-17 10:55:43 +0200

    Removed Given Step

--- src/test/features/api/User Comments.feature
+++ src/test/features/api/User Comments.feature
@@ -2,8 +2,7 @@ Feature: Receive User Comment
 
   @regression
   Scenario Outline: Receive User Comments
-    Given the url
-    And Search for the user with username “<Username>”
+    When Search for the user with username “<Username>”
     Then Successfully get id and comments
 
     Examples:
--- src/test/java/platforms/api/StepDefinitions/UsersCommentsSteps.java
+++ src/test/java/platforms/api/StepDefinitions/UsersCommentsSteps.java
@@ -1,16 +1,15 @@
 package platforms.api.StepDefinitions;
 
-import cucumber.api.java.en.And;
+
 import cucumber.api.java.en.Given;
+import cucumber.api.java.en.When;
+import cucumber.api.java.en.And;
 import cucumber.api.java.en.Then;
 import platforms.api.UserComments;
 
 public class UsersCommentsSteps {
-    @Given("^the url$")
-    public void the_url() {
-    }
 
-    @And("^Search for the user with username “(.+)”$$")
+    @When("^Search for the user with username “(.+)”$$")
     public void searchForTheUserWithUsername(String username) {
         UserComments.getUsername(username);
     }
